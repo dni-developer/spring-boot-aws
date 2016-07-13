03b070d7373e9ea12e304176a82f34756974fa0d
dni.developer <dni.developer@hotmail.com>
2016-07-12 22:03:09 -0400

    add multiple file upload

--- src/main/java/net/dni/rest/AmazonS3RestController.java
+++ src/main/java/net/dni/rest/AmazonS3RestController.java
@@ -1,7 +1,10 @@
 package net.dni.rest;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.util.IOUtils;
 import net.dni.Loggable;
@@ -9,10 +12,13 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @RestController
 @RequestMapping("/rest")
@@ -27,19 +33,68 @@ public class AmazonS3RestController {
     @Loggable
     private Logger logger;
 
+    /**
+     * List objects in default bucket
+     *
+     * @return
+     */
     @RequestMapping(value = "/list", method = RequestMethod.GET, produces = "application/json")
     public ObjectListing list() {
         return amazonS3.listObjects(bucketName);
     }
 
-    @RequestMapping(value = "/view/{key}/", method = RequestMethod.GET, produces = "application/json")
-    public S3Object view(@PathVariable("key") String key) {
+    /**
+     * View metadata of object in default bucket
+     *
+     * @param key filename
+     * @return
+     */
+    @RequestMapping(value = "/metadata/{key}/", method = RequestMethod.GET, produces = "application/json")
+    public S3Object metadata(@PathVariable("key") String key) {
         logger.info("key: {}", key);
         return amazonS3.getObject(bucketName, key);
     }
 
+    /**
+     * delete object in default bucket
+     *
+     * @param key filename
+     */
+    @RequestMapping(value = "/delete/{key}/", method = RequestMethod.DELETE, produces = "application/json")
+    public void delete(@PathVariable("key") String key) {
+        logger.info("key: {}", key);
+        amazonS3.deleteObject(bucketName, key);
+    }
+
+    /**
+     * Read object content as String in default bucket
+     *
+     * @param key
+     * @return
+     * @throws IOException
+     */
     @RequestMapping(value = "/download/{key}/", method = RequestMethod.GET)
-    public void download(@PathVariable("key") String key, HttpServletResponse response) throws IOException {
+    public String download(@PathVariable("key") String key) throws IOException {
+        logger.info("key: {}", key);
+        S3Object object = amazonS3.getObject(bucketName, key);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(object.getObjectContent()));
+        StringBuilder content = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            content.append(line);
+        }
+        return content.toString();
+    }
+
+    /**
+     * Stream object content in default bucket
+     *
+     * @param key      filename
+     * @param response HttpResponse
+     * @throws IOException
+     */
+    @RequestMapping(value = "/stream/download/{key}/", method = RequestMethod.GET)
+    public void streamDownload(@PathVariable("key") String key, HttpServletResponse response) throws IOException {
         logger.info("key: {}", key);
         S3Object object = amazonS3.getObject(bucketName, key);
 
@@ -50,4 +105,30 @@ public class AmazonS3RestController {
         IOUtils.copy(stream, response.getOutputStream());
         response.flushBuffer();
     }
+
+    /**
+     * Stream upload multipart files to default bucket
+     * add User metadata
+     * @param files
+     * @throws IOException
+     */
+    @RequestMapping(value = "/stream/upload", method = RequestMethod.POST)
+    public void streamUploadMultipleFile(@RequestParam("file") MultipartFile[] files) throws IOException {
+        for (MultipartFile file : files) {
+            logger.info("fileupload:{}, size:{}", file.getOriginalFilename(), file.getSize());
+            if (!file.isEmpty()) {
+                try {
+                    ObjectMetadata metadata = new ObjectMetadata();
+                    metadata.setContentLength(file.getSize());
+                    metadata.setContentType(file.getContentType());
+                    metadata.addUserMetadata("user", "dni");
+                    PutObjectRequest request = new PutObjectRequest(bucketName, file.getOriginalFilename(), file.getInputStream(), metadata);
+                    amazonS3.putObject(request);
+                } catch (AmazonServiceException e){
+                    logger.error("",e);
+                }
+            }
+        }
+    }
+
 }
